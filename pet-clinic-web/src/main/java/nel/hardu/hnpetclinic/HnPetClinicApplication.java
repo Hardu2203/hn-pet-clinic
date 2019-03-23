bd76abaaf1fd9727ecff19ae569804b0f805b353
Hardu2203 <hardunelhn@gmail.com>
2019-03-24 00:24:40 +0200

    Added Index page and Controller. Closes #8.

--- pet-clinic-web/src/main/java/nel/hardu/hnpetclinic/HnPetClinicApplication.java
+++ pet-clinic-web/src/main/java/nel/hardu/hnpetclinic/HnPetClinicApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HnPetClinicApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(HnPetClinicApplication.class, args);
 	}
 
