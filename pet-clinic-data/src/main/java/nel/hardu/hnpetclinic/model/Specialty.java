d3de4d46831b81c859e4be57d74bf46563ab7cd4
Hardu2203 <hardunelhn@gmail.com>
2019-08-04 10:31:12 +0200

    Refactor to include lombok. Closes #52

--- pet-clinic-data/src/main/java/nel/hardu/hnpetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/nel/hardu/hnpetclinic/model/Specialty.java
@@ -1,22 +1,21 @@
 package nel.hardu.hnpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Table(name = "specialties")
 public class Specialty extends BaseEntity{
 
     @Column(name = "description")
     private String description;
 
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
--- pet-clinic-data/src/main/java/nel/hardu/hnpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/nel/hardu/hnpetclinic/model/Visit.java
@@ -1,13 +1,20 @@
 package nel.hardu.hnpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Table(name = "visits")
 public class Visit extends BaseEntity{
 
-    @Column(name = "Date")
+    @Column(name = "date")
     private LocalDate date;
 
     @Column(name = "description")
@@ -17,27 +24,4 @@ public class Visit extends BaseEntity{
     @JoinColumn(name = "pet_id")
     private Pet pet;
 
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Pet getPet() {
-        return pet;
-    }
-
-    public void setPet(Pet pet) {
-        this.pet = pet;
-    }
 }
